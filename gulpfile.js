edbe89b22c05202c66deb7a1d6d6edb83a530719
Jerome WAX <jerome.wax@lo2k.net>
2015-03-04 14:28:30 +0100

    huge dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var exec = require('gulp-exec');
 var clean = require('gulp-clean');
+var md5 = require('MD5');
+var fs = require('fs');
+var path = require('path');
 
 
 var paths = {
-	pdf: 	'workspace/**.pdf',
+	pdf: 'workspace/**.pdf',
 	images: 'workspace/**.jpg',
 	texts: 'workspace/**.txt'
 };
@@ -17,8 +20,7 @@ gulp.task('watch', function() {
 	return gulp.watch(paths.pdf, ['extractPDF']);
 });
 
-gulp.task('handlePDF', ['clean','extractImages'], function() {
-})
+gulp.task('handlePDF', ['clean', 'extractImages'], function() {})
 
 
 gulp.task('clean', function() {
@@ -28,63 +30,63 @@ gulp.task('clean', function() {
 
 gulp.task('extractPDF', function() {
 	var options = {
-	    continueOnError: false, // default = false, true means don't emit error event 
-	    pipeStdout: false // default = false, true means stdout is written to file.contents 
-	    //customTemplatingThing: "test" // content passed to gutil.template() 
+		continueOnError: false, // default = false, true means don't emit error event 
+		pipeStdout: false // default = false, true means stdout is written to file.contents 
+		//customTemplatingThing: "test" // content passed to gutil.template() 
 	};
 
 	var reportOptions = {
-	    err: true, // default = true, false means don't write err 
-	    stderr: true, // default = true, false means don't write stderr 
-	    stdout: true // default = true, false means don't write stdout 
+		err: true, // default = true, false means don't write err 
+		stderr: true, // default = true, false means don't write stderr 
+		stdout: true // default = true, false means don't write stdout 
 	}
 
 
 
 	return gulp.src(paths.pdf)
-	.pipe(exec('convert -density 300 <%= file.path%> <%= file.path%>.jpg'))    
-	.pipe(exec.reporter(reportOptions))
-	.on('finish', function() {
-		gulp.src(paths.pdf)
-		.pipe(gulp.dest('build/'))
+		.pipe(exec('convert -density 300 <%= file.path%> <%= file.path%>.jpg'))
+		.pipe(exec.reporter(reportOptions))
+		.on('finish', function() {
+			gulp.src(paths.pdf)
+				.pipe(gulp.dest('build/'))
 
-		gulp.src(paths.pdf)
-		.pipe(clean());
-	});
+			gulp.src(paths.pdf)
+				.pipe(clean());
+		});
 });
 
 gulp.task('extractImages', ['extractPDF'], function() {
 	var options = {
-	    continueOnError: false, // default = false, true means don't emit error event 
-	    pipeStdout: false // default = false, true means stdout is written to file.contents 
-	    //customTemplatingThing: "test" // content passed to gutil.template() 
+		continueOnError: false, // default = false, true means don't emit error event 
+		pipeStdout: false // default = false, true means stdout is written to file.contents 
+		//customTemplatingThing: "test" // content passed to gutil.template() 
 	};
 
 	var reportOptions = {
-	    err: true, // default = true, false means don't write err 
-	    stderr: true, // default = true, false means don't write stderr 
-	    stdout: true // default = true, false means don't write stdout 
+		err: true, // default = true, false means don't write err 
+		stderr: true, // default = true, false means don't write stderr 
+		stdout: true // default = true, false means don't write stdout 
 	}
 
 	return gulp.src(paths.images)
-	.pipe(exec('tesseract <%= file.path%> <%= file.path%> -l eng'))    
-    //.pipe(exec.reporter(reportOptions))
-    .on('finish', function() {
-            /*gulp.src(paths.images)
+		.pipe(exec('tesseract <%= file.path%> <%= file.path%> -l eng'))
+		//.pipe(exec.reporter(reportOptions))
+		.on('finish', function() {
+			/*gulp.src(paths.images)
    			.pipe(gulp.dest('build/'))
    	
 		   	gulp.src(paths.images)
 		   	.pipe(clean());*/
-		   });
+		});
 });
 
 //for debug purpose
 gulp.task('restore', function() {
 	gulp.src('build/**.pdf')
-	.pipe(gulp.dest('workspace/'));
+		.pipe(gulp.dest('workspace/'));
 
 	gulp.src('build/**.*')
-	.pipe(clean());
+		.pipe(clean());
 });
 
 
@@ -95,23 +97,58 @@ function sendToElastichSearch() {
 		log: 'trace'
 	});
 
-  // you're going to receive Vinyl files as chunks
-  function transform(file, cb) {
-    // read and modify file contents
-    file.contents = new Buffer(String(file.contents));
+	function readImageCb(file, buf) {
 
-    var text = file.contents.toString('UTF-8');
-    var filename = file.path;
-    var picturefile = file.path.replace(".txt","");
-
-    var filename = path.filename(file.path);
+		//init variables
+		var text = file.contents.toString('UTF-8');
+		var textfile = file.path;
+		var picturefile = file.path.replace(".txt", "");
+		var checksum = md5(buf);
+		var filename = path.basename(picturefile);
 
 
-    console.log();
-    console.log("============");
-    console.log(file.path);
+		client.create({
+			index: 'myindex',
+			type: 'mytype',
+			body: {
+				content: text,
+				id: checksum
 
- 	/*client.create({
+			}
+		}, function(error, response) {
+			console.log(response);
+		})
+
+		client.create({
+			index: 'pictures',
+			type: 'mytype',
+			body: {
+				content: buf.toString('base64'),
+				id: checksum
+
+			}
+		}, function(error, response) {
+			console.log(response);
+		})
+	}
+
+	// you're going to receive Vinyl files as chunks
+	function transform(file, cb) {
+		// read and modify file contents
+		file.contents = new Buffer(String(file.contents));
+
+		var picturefile = file.path.replace(".txt", "");
+
+
+		console.log();
+		console.log("============");
+		console.log(file.path);
+
+		fs.readFile(picturefile, function(err, buf) {
+			readImageCb(file, buf)
+		});
+
+		/*client.create({
     	index: 'myindex',
     	type: 'mytype',
     	body: {
@@ -126,20 +163,18 @@ function sendToElastichSearch() {
     	console.log(response);
     });*/
 
-cb(null, file);
+		cb(null, file);
 
 
-}
-return require('event-stream').map(transform);
+	}
+	return require('event-stream').map(transform);
 }
 
 gulp.task('elastic', [], function() {
-	
+
 
 
 	gulp.src(paths.texts)
-	.pipe(sendToElastichSearch());
+		.pipe(sendToElastichSearch());
 
 });
-
-
